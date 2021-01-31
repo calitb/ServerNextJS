09193b8c0ecb2d190a090e3d22320e80a4a628ca
Carlos Thurber B <thurber.carlos@gmail.com>
2021-01-31 00:46:12 -0500

    Adds localization

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    i18n: {
+      locales: ['en', 'es'],
+      defaultLocale: 'es',
+    },
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
+import { GetStaticProps } from 'next';
 import styles from '@/styles/Home.module.css';
 
-export default function Home() {
+interface Props {
+  message: string;
+}
+
+export default function Home({ message }: Props) {
   return (
     <div className={styles.container}>
       <div className={styles.overlay}></div>
       <div className="absolute h-full w-full p-16 uppercase">
         <h1 className="text-5xl font-bold my-8">calitb.dev</h1>
-        <p className={`text-terminal-green opacity-80 ${styles['terminal-shadow']}`}>&gt; This page is under construction</p>
+        <p className={`text-terminal-green opacity-80 ${styles['terminal-shadow']}`}>&gt; {message}</p>
       </div>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => {
+  const STRINGS = {
+    en: 'This page is under construction',
+    es: 'Esta página está en construcción',
+  };
+
+  console.log({ locale });
+
+  return { props: { message: STRINGS[locale] ?? STRINGS['en'] } };
+};
