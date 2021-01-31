780291107274bf9fea822d5734069dd219071e7b
Carlos Thurber B <thurber.carlos@gmail.com>
2021-01-31 01:31:09 -0500

    add graphql endpoint

---
+++ pages/api/graphql.ts
@@ -0,0 +1,75 @@
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+import { get } from '@/utils/common';
+
+const typeDefs = gql`
+  type Query {
+    metrobus(user: String!): Result!
+    corredor(user: String!): Result!
+  }
+  type Datetime {
+    date: String!
+    time: String
+    type: String!
+  }
+  type Details {
+    model: String
+    ksi: String
+    fechalogueoString: String
+    waste: String
+    water: String
+    montoVencido30: String
+    montoVencido60: String
+    montoActual: String
+    numCuenta: String
+  }
+  type Result {
+    """
+    Status of the operation: 'success' or 'error'
+    """
+    status: String!
+    """
+    Error message
+    """
+    msg: String
+    """
+    Account balance
+    """
+    balance: String
+    datetime: Datetime
+    details: Details
+  }
+`;
+
+const promiseWrapper = (url: string, params: Record<string, any>): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    get(url, params, undefined, undefined, (response) => {
+      resolve(response);
+    });
+  });
+};
+
+const resolvers = {
+  Query: {
+    metrobus: async (parent, args, context) => {
+      const url = 'http://localhost:3000/api/v2/metrobus';
+      const response = await promiseWrapper(url, args);
+      return JSON.parse(response);
+    },
+    corredor: async (parent, args, context) => {
+      const url = 'http://localhost:3000/api/v2/corredor';
+      const response = await promiseWrapper(url, args);
+      return JSON.parse(response);
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
