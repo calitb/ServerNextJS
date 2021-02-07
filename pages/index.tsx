e7752bd2a3417be2ac1ef790e27263573a5182df
Carlos Thurber B <thurber.carlos@gmail.com>
2021-02-07 18:19:11 -0500

    Adds logic to deploy image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 
 interface Props {
@@ -7,13 +8,18 @@ interface Props {
 
 export default function Home({ message }: Props) {
   return (
-    <div className={styles.container}>
-      <div className={styles.overlay}></div>
-      <div className="absolute h-full w-full p-16 uppercase">
-        <h1 className="text-5xl font-bold my-8">calitb.dev</h1>
-        <p className={`text-terminal-green opacity-80 ${styles['terminal-shadow']}`}>&gt; {message}</p>
+    <>
+      <Head>
+        <title>calitb.dev</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.overlay}></div>
+        <div className="absolute h-full w-full p-16 uppercase">
+          <h1 className="text-5xl font-bold my-8">calitb.dev</h1>
+          <p className={`text-terminal-green opacity-80 ${styles['terminal-shadow']}`}>&gt; {message}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
