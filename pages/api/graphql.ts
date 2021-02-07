5b1cb7fca2cb43bff08f1b5b961923df0fb20e1a
Carlos Thurber B <thurber.carlos@gmail.com>
2021-02-07 17:02:55 -0500

    Enables graphql playground in production

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -166,7 +166,7 @@ const resolvers = {
   },
 };
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({ typeDefs, resolvers, playground: true });
 
 export const config = {
   api: {
