c02fa50961420c356bdd153f6c0319d18b00e37f
Carlos Thurber B <thurber.carlos@gmail.com>
2021-02-13 10:43:46 -0500

    removes question about specific content in e2e

--- cypress/integration/repositories.js
+++ cypress/integration/repositories.js
@@ -10,19 +10,37 @@ describe('Repositories Page', () => {
         cy.get('#link-Repositories').should('have.css', 'background-color', 'rgb(17, 24, 39)');
       });
 
-      it('should display the right amount of cards', () => {
-        cy.get('main').children().should('have.length', 12);
-      });
+      it('should display the cards', () => {
+        cy.get('main').children().its('length').should('be.gte', 0);
 
-      it('should display the right content in the card', () => {
-        cy.get('#card-0 > .w-full > .justify-between > :nth-child(1) > #summary > #name').should('contain', 'GithubWorkflow');
-        cy.get('#card-1 > .w-full > .justify-between > :nth-child(1) > #summary > #description').should('contain', 'Sample of ReactNative app fetching graphql');
+        cy.get('main')
+          .children()
+          .each(($el, index) => {
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #summary > #name`)
+              .invoke('text')
+              .then((text) => {
+                expect(text.length).to.be.at.least(1);
+              });
 
-        cy.get('#card-2 > .w-full > .justify-between > :nth-child(1) > #techs > #tech-0').should('contain', 'graphql-server');
-        cy.get('#card-2 > .w-full > .justify-between > :nth-child(1) > #techs > #tech-1').should('contain', 'apollographql');
-        cy.get('#card-2 > .w-full > .justify-between > :nth-child(1) > #techs > #tech-2').should('contain', 'nodejs');
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #summary > #description`)
+              .invoke('text')
+              .then((text) => {
+                expect(text.length).to.be.at.least(1);
+              });
 
-        cy.get('#card-3 > .w-full > .justify-between > :nth-child(2) > #button > a').should('have.attr', 'href', 'https://github.com/calitb/Sample-NextJS').should('have.attr', 'target', '_blank');
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(2) > #button > a`).should('have.attr', 'href').and('contain', 'https://github.com/calitb/');
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(2) > #button > a`).should('have.attr', 'target', '_blank');
+
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #techs`)
+              .children()
+              .each(($el, indexTopic) => {
+                cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #techs > #tech-${indexTopic}`)
+                  .invoke('text')
+                  .then((text) => {
+                    expect(text.length).to.be.at.least(1);
+                  });
+              });
+          });
       });
     });
 
@@ -45,19 +63,37 @@ describe('Repositories Page', () => {
         cy.get('#link-Repositories').should('have.css', 'background-color', 'rgb(17, 24, 39)');
       });
 
-      it('should display the right amount of cards', () => {
-        cy.get('main').children().should('have.length', 12);
-      });
+      it('should display the cards', () => {
+        cy.get('main').children().its('length').should('be.gte', 0);
 
-      it('should display the right content in the card', () => {
-        cy.get('#card-0 > .w-full > .justify-between > :nth-child(1) > #summary > #name').should('contain', 'GithubWorkflow');
-        cy.get('#card-1 > .w-full > .justify-between > :nth-child(1) > #summary > #description').should('contain', 'Sample of ReactNative app fetching graphql');
+        cy.get('main')
+          .children()
+          .each(($el, index) => {
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #summary > #name`)
+              .invoke('text')
+              .then((text) => {
+                expect(text.length).to.be.at.least(1);
+              });
 
-        cy.get('#card-2 > .w-full > .justify-between > :nth-child(1) > #techs > #tech-0').should('contain', 'graphql-server');
-        cy.get('#card-2 > .w-full > .justify-between > :nth-child(1) > #techs > #tech-1').should('contain', 'apollographql');
-        cy.get('#card-2 > .w-full > .justify-between > :nth-child(1) > #techs > #tech-2').should('contain', 'nodejs');
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #summary > #description`)
+              .invoke('text')
+              .then((text) => {
+                expect(text.length).to.be.at.least(1);
+              });
 
-        cy.get('#card-3 > .w-full > .justify-between > :nth-child(2) > #button > a').should('have.attr', 'href', 'https://github.com/calitb/Sample-NextJS').should('have.attr', 'target', '_blank');
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(2) > #button > a`).should('have.attr', 'href').and('contain', 'https://github.com/calitb/');
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(2) > #button > a`).should('have.attr', 'target', '_blank');
+
+            cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #techs`)
+              .children()
+              .each(($el, indexTopic) => {
+                cy.get(`#card-${index} > .w-full > .justify-between > :nth-child(1) > #techs > #tech-${indexTopic}`)
+                  .invoke('text')
+                  .then((text) => {
+                    expect(text.length).to.be.at.least(1);
+                  });
+              });
+          });
       });
     });
 
