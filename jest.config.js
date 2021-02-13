0f9d3ac93fc49fd811940e9ed15d70dce16325e1
Carlos Thurber <thurber.carlos@gmail.com>
2021-02-13 11:53:44 -0500

    Implements jest (#6)

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '@/components/(.*)': '<rootDir>/components/$1',
+    '@/pages/(.*)': '<rootDir>/pages/$1',
+    '@/github/(.*)': '<rootDir>/github/$1',
+    '@/utils/(.*)': '<rootDir>/utils/$1',
+    '@/styles/(.*)': '<rootDir>/styles/$1',
+  },
+  setupFilesAfterEnv: [],
+};
