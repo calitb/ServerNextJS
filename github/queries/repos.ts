d35cfd8601b5daf16c15923f263b757a2b53cda5
Carlos Thurber B <thurber.carlos@gmail.com>
2021-02-13 01:43:29 -0500

    Reduces complexity of graphql query

--- github/queries/repos.ts
+++ github/queries/repos.ts
@@ -3,7 +3,7 @@ import { gql } from 'apollo-boost';
 export default gql`
   query Repos {
     viewer {
-      repositories(orderBy: { field: NAME, direction: ASC }, first: 50, privacy: PUBLIC, isFork: false) {
+      repositories(orderBy: { field: NAME, direction: ASC }, first: 16, privacy: PUBLIC, isFork: false) {
         edges {
           node {
             __typename
@@ -12,7 +12,7 @@ export default gql`
               name
               url
               description
-              repositoryTopics(first: 50) {
+              repositoryTopics(first: 6) {
                 nodes {
                   topic {
                     name
