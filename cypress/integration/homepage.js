e90a847c3bcb0cb259634136bdf4e9e68f3f8c52
Carlos Thurber B <thurber.carlos@gmail.com>
2021-02-12 23:43:32 -0500

    Runs e2e

---
+++ cypress/integration/homepage.js
@@ -0,0 +1,97 @@
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  context('Desktop', () => {
+    context('Content', () => {
+      it('should display the title', () => {
+        cy.get('#title').should('contain', 'calitb.dev');
+      });
+
+      it('should display the message', () => {
+        cy.get('#message').should('contain', 'Esta página está en construcción');
+      });
+
+      it('should display links as not selected', () => {
+        cy.get('#link-Repositories').should('have.css', 'color', 'rgb(209, 213, 219)');
+        cy.get('#link-Repositories').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+      });
+    });
+
+    context('Navigation', () => {
+      it('should redirect to the repositories page', () => {
+        cy.get('#link-Repositories').click().url().should('contain', '/repositories');
+      });
+
+      it('should redirect to the homepage', () => {
+        cy.get('#link-Repositories').click().url().should('contain', '/repositories');
+        cy.get('#link-home')
+          .click()
+          .url()
+          .should('eq', Cypress.config().baseUrl + '/');
+      });
+
+      it('should redirect to the github page', () => {
+        cy.get('#link-github').should('have.attr', 'href', 'https://github.com/calitb').should('have.attr', 'target', '_blank');
+        cy.request('https://github.com/calitb').its('status').should('eq', 200);
+      });
+    });
+  });
+
+  context('Mobile', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-xr');
+    });
+
+    context('Content', () => {
+      it('should display the title', () => {
+        cy.get('#title').should('contain', 'calitb.dev');
+      });
+
+      it('should display the message', () => {
+        cy.get('#message').should('contain', 'Esta página está en construcción');
+      });
+
+      it('should display links as not selected', () => {
+        cy.get('#mobile-menu-icon-open').click();
+        cy.get('#mlink-Repositories').should('have.css', 'color', 'rgb(209, 213, 219)');
+        cy.get('#mlink-Repositories').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+      });
+    });
+
+    context('Menu', () => {
+      it('should open and close the menu', () => {
+        cy.get('#mobile-menu-icon-close').should('have.css', 'display', 'none');
+        cy.get('#mobile-menu-icon-open').should('have.css', 'display', 'block');
+        cy.get('#mobile-menu-icon-open').click();
+        cy.get('#mobile-menu-icon-open').should('have.css', 'display', 'none');
+        cy.get('#mobile-menu-icon-close').should('have.css', 'display', 'block');
+        cy.get('#mobile-menu-icon-close').click();
+        cy.get('#mobile-menu-icon-close').should('have.css', 'display', 'none');
+        cy.get('#mobile-menu-icon-open').should('have.css', 'display', 'block');
+      });
+    });
+
+    context('Navigation', () => {
+      it('should redirect to the repositories page', () => {
+        cy.get('#mobile-menu-icon-open').click();
+        cy.get('#mlink-Repositories').click().url().should('contain', '/repositories');
+      });
+
+      it('should redirect to the homepage', () => {
+        cy.get('#mobile-menu-icon-open').click();
+        cy.get('#mlink-Repositories').click().url().should('contain', '/repositories');
+        cy.get('#link-home')
+          .click()
+          .url()
+          .should('eq', Cypress.config().baseUrl + '/');
+      });
+
+      it('should redirect to the github page', () => {
+        cy.get('#link-github').should('have.attr', 'href', 'https://github.com/calitb').should('have.attr', 'target', '_blank');
+        cy.request('https://github.com/calitb').its('status').should('eq', 200);
+      });
+    });
+  });
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,12 @@ export default function Home({ message }: Props) {
         <main>
           <div className={styles.overlay}></div>
           <div className="absolute h-full w-full p-16 uppercase">
-            <h1 className="text-5xl font-bold my-8">calitb.dev</h1>
-            <p className={`text-terminal-green opacity-80 ${styles['terminal-shadow']}`}>&gt; {message}</p>
+            <h1 id="title" className="text-5xl font-bold my-8">
+              calitb.dev
+            </h1>
+            <p id="message" className={`text-terminal-green opacity-80 ${styles['terminal-shadow']}`}>
+              &gt; {message}
+            </p>
           </div>
         </main>
       </div>
