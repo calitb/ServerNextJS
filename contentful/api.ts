095b2bf3dee67887cc9d96b1007769806c190056
Carlos Thurber B <thurber.carlos@gmail.com>
2021-02-20 17:04:23 -0500

    improves timestamp to hide the old movies

--- contentful/api.ts
+++ contentful/api.ts
@@ -5,12 +5,12 @@ import { contentfulGraphQlClient as gqClient } from './client';
 import movies from './queries/movies';
 
 export const getMovies = async (): Promise<MoviesPageView[]> => {
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
+  const date = new Date();
+  date.setHours(date.getHours() - 4);
 
   const { data } = await gqClient.query<MoviesPage>({
     query: movies,
-    variables: { date: yesterday.toISOString() },
+    variables: { date: date.toISOString() },
   });
 
   return new MoviesMapper().map(data);
