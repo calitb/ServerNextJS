da38f7a0dd8566d125744bc55e4bf7036131527b
Carlos Thurber B <thurber.carlos@gmail.com>
2021-02-25 21:15:48 -0500

    Adds sitemap generation

---
+++ scripts/sitemap.ts
@@ -0,0 +1,49 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+
+import { writeFile } from 'fs';
+
+(async () => {
+  try {
+    const smStream = new SitemapStream({
+      hostname: 'https://calitb.dev',
+    });
+
+    const urls = buildURLs();
+
+    urls.forEach(({ url, priority }) => {
+      smStream.write({
+        url,
+        changefreq: 'weekly',
+        priority,
+      });
+    });
+
+    // End sitemap stream
+    smStream.end();
+
+    // XML sitemap string
+    const sitemapOutput = (await streamToPromise(smStream)).toString();
+
+    writeFile('public/sitemap.xml', sitemapOutput, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      // eslint-disable-next-line no-console
+      console.log('Sitemap generated!');
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('Failed to generate the sitemap');
+  }
+})();
+
+export {};
+
+function buildURLs(): Array<{ url: string; priority: number }> {
+  const fixedURLs = [
+    { url: '', priority: 0.5 },
+    { url: 'repositories', priority: 1.0 },
+  ];
+
+  return fixedURLs;
+}
