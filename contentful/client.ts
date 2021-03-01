17d5586927b30bfa54e0c62acb0348a90784b82f
Carlos Thurber B <thurber.carlos@gmail.com>
2021-03-01 09:27:24 -0500

    small changes

--- contentful/client.ts
+++ contentful/client.ts
@@ -3,7 +3,7 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_ENVIRONMENT } = process.env;
 
 export const contentfulGraphQlClient = new ApolloClient({
-  uri: `https://graphql.contentful.com/content/v1/` + `spaces/${CONTENTFUL_SPACE_ID}/` + `environments/${CONTENTFUL_ENVIRONMENT}?` + `access_token=${CONTENTFUL_ACCESS_TOKEN}`,
+  uri: `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}/environments/${CONTENTFUL_ENVIRONMENT}?access_token=${CONTENTFUL_ACCESS_TOKEN}`,
   cache: new InMemoryCache({
     addTypename: false,
   }),
