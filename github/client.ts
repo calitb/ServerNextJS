0e50ab76cfef816e6d9b437b156fe0f582171388
Carlos Thurber <thurber.carlos@gmail.com>
2021-03-01 09:36:55 -0500

    Clean up graphql clients code (#16)

--- github/client.ts
+++ github/client.ts
@@ -1,16 +1,6 @@
-import ApolloClient, { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-boost';
-
-import fetch from 'cross-fetch';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 const { API_GITHUB_PERSONAL_ACCESS_TOKEN } = process.env;
-
-const fragmentMatcher = new IntrospectionFragmentMatcher({
-  introspectionQueryResultData: {
-    __schema: {
-      types: [], // no types provided
-    },
-  },
-});
 
 export const githubGraphQlClient = new ApolloClient({
   uri: 'https://api.github.com/graphql',
@@ -21,9 +11,7 @@ export const githubGraphQlClient = new ApolloClient({
       },
     });
   },
-  fetch,
   cache: new InMemoryCache({
     addTypename: false,
-    fragmentMatcher,
   }),
 });
