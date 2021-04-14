3299d040d9ed376278100fcef5f09d3dbbe88ef3
Carlos Thurber <carlos.thurber@admios-sa.com>
2021-04-14 14:40:57 -0500

    Display download urls in a modal when needed

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
-export default (): JSX.Element => {
+export default function HTTP404(): JSX.Element {
   return (
     <>
       <Head>
@@ -14,7 +14,10 @@ export default (): JSX.Element => {
           <h2 className="mt-1">Page not found</h2>
         </header>
         <div className="flex flex-col items-center text-center mt-6">
-          <div className="max-w-sm md:max-w-md px-4 md:px-0">Sorry we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Or the page might be unavailable.</div>
+          <div className="max-w-sm md:max-w-md px-4 md:px-0">
+            Sorry we couldn’t find the page you’re looking for. Perhaps you’ve
+            mistyped the URL? Or the page might be unavailable.
+          </div>
           <div className="mt-8">
             <Link href="/">
               <a className="block tracking-widest uppercase text-center shadow bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded">
@@ -26,4 +29,4 @@ export default (): JSX.Element => {
       </main>
     </>
   );
-};
+}
