23dc718bccea893608665c2f0a64bbd4edd56bc2
Carlos Thurber <thurber.carlos@gmail.com>
2021-12-30 18:09:46 -0500

    adds extra email

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,7 +1,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { FC } from "react";
 
-const VALID_USERS = ["thurber.carlos@gmail.com", "abarsallo26@gmail.com"];
+const VALID_USERS = ["thurber.carlos@gmail.com", "abarsallo26@gmail.com", "abarsallo.dev@gmail.com"];
 
 export const AuthWrapper: FC = ({ children }) => {
   const { data: session } = useSession();
