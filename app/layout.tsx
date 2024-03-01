9995e67af05d07f213978b41575de5bc4b042fb1
Carlos Thurber <thurber.carlos@gmail.com>
2024-03-01 11:56:44 -0500

    adds placeholder for home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
       <body>
         <div className="flex max-h-screen min-h-screen min-w-[100vw] max-w-[100vw] flex-col bg-gray-100">
           <Navbar />
-          {children}
+          <div className="flex h-[calc(100vh-theme(height.header))] flex-col">
+            {children}
+          </div>
         </div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -6,5 +6,15 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <main className="flex">content</main>;
+  return (
+    <main className="home-container relative h-full text-2xl text-[#80ff80cc;] ">
+      <div className="overlay absolute h-full w-full"></div>
+      <div className="flex-1 p-16 uppercase">
+        <h1 className="my-8 text-5xl font-bold">calitb.dev</h1>
+        <p className="text-terminal-green terminal-shadow opacity-80">
+          &gt; This page is under construction
+        </p>
+      </div>
+    </main>
+  );
 }
